f2c98b6fe7ed41cb4590b1bfc50845129026283a
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2026-02-19 19:05:31 +0530

    main: test mini vm

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
-pub mod vm;
+mod vm;
+
+use vm::{ExecResult, MiniVm};
+
+fn main() {
+    let code = vec![0x60, 0x03, 0x60, 0x05, 0x01, 0x00];
+
+    let mut vm = MiniVm::new(code);
+    let result = vm.run();
+
+    println!("Result: {:?}", result);
+    println!("Top of stack: {:?}", vm.peek());
+}
